a3df17202e34939bf938fd1b5100af87671040a6
chikara10 <chikara140901@docomo.ne.jp>
2025-11-01 14:52:10 +0900

    firdt commit

---
+++ fuel/app/logs/2025/11/01.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2025-11-01 14:22:44 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-11-01 14:31:56 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
